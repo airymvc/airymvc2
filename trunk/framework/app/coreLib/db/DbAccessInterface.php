acc2152f4eb7024f8e9fc2085a6900f59a243a34
aaronchang.tw@gmail.com <aaronchang.tw@gmail.com@664e361d-b696-899b-ad6e-09a71d936bb0>
2013-06-27 06:20:41 +0000

    add comments and refactoring

---
+++ trunk/framework/app/coreLib/db/DbAccessInterface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ *
+ */
+
+interface DbAccessInterface {
+	
+    public function select($columns, $table, $distinct = 0);
+
+    public function update($columns, $table);
+    
+    public function insert($columns, $table);  
+    
+    public function delete($table);
+    
+    public function execute();
+    
+    public function getStatement();
+    
+    public function groupBy($column);
+    
+    public function joinOn($condition);
+    
+    public function limit($offset, $interval);
+
+    public function orderBy($column, $if_desc = NULL);
+
+	
+	
+}
