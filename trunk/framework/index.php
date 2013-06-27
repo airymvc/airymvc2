38fb64d999bd4834b29bb143a839f97ea98a5c24
aaronchang.tw@gmail.com <aaronchang.tw@gmail.com@664e361d-b696-899b-ad6e-09a71d936bb0>
2013-06-27 06:25:59 +0000

    add comments and refactoring

--- trunk/framework/index.php
+++ trunk/framework/index.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ *
+ */
+
+
 	include("core".DIRECTORY_SEPARATOR."Ini.php");
 	Initializer::initialize();
 	$Router = Loader::load("Router");
