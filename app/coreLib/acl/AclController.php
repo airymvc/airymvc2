85346a61287d7c79209fc22ae5b14bf912647e3c
aaronchang.tw <aaronchang.tw@gmail.com>
2013-06-27 06:21:06 +0000

    add comments and refactoring

--- app/coreLib/acl/AclController.php
+++ app/coreLib/acl/AclController.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ */
 
 /*
  * @TODO: Need to refactoring this. This controller shares several methods with AppController
--- app/coreLib/acl/Authentication.php
+++ app/coreLib/acl/Authentication.php
@@ -1,6 +1,17 @@
 <?php
 
-//require_once ('../config/lib/AclUtility.php');
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ */
+
 
 class Authentication {
 
