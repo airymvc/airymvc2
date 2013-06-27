e9859fa0839a116e106420af1738b1f1562470ed
aaronchang.tw@gmail.com <aaronchang.tw@gmail.com@664e361d-b696-899b-ad6e-09a71d936bb0>
2013-06-27 06:23:08 +0000

    add comments and refactoring

--- trunk/framework/app/coreLib/view/StreamHelper.php
+++ trunk/framework/app/coreLib/view/StreamHelper.php
@@ -1,9 +1,18 @@
 <?php
 
-/*
- * This slass is used for write or read stream for the view
- * Referring to StreamWrapper in PHP
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ *
  */
+
 Class StreamHelper {
     /**
      * current stream position
--- trunk/framework/app/coreLib/view/VariableHelper.php
+++ trunk/framework/app/coreLib/view/VariableHelper.php
@@ -1,6 +1,16 @@
 <?php
 
-/*
- * VariableHelper defines variables that will be used in the view
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ *
  */
+
 ?>
