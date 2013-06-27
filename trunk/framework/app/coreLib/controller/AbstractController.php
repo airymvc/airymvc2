8e8f7e2a94e62c81d3c4d489a0104b2dd6d53f38
aaronchang.tw@gmail.com <aaronchang.tw@gmail.com@664e361d-b696-899b-ad6e-09a71d936bb0>
2013-06-27 06:21:36 +0000

    add comments and refactoring

--- trunk/framework/app/coreLib/controller/AbstractController.php
+++ trunk/framework/app/coreLib/controller/AbstractController.php
@@ -1,5 +1,18 @@
 <?php
 /**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ *
+ */
+
+/**
  * Description of AbstractController
  *
  * @author Hung-Fu Aaron Chang
--- trunk/framework/app/coreLib/db/MysqlAccess.php
+++ trunk/framework/app/coreLib/db/MysqlAccess.php
@@ -1,10 +1,18 @@
 <?php
 
-/*
- *  Sep. 2, version for inner join function
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
  */
 
-class MysqlAccess  {
+class MysqlAccess implements DbAccessInterface {
 
     private $dbConfig_array;
     private $query_stmt;
