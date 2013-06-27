086d6d32594b3369d9ebb22e5dee2f08e595c5f0
aaronchang.tw@gmail.com <aaronchang.tw@gmail.com@664e361d-b696-899b-ad6e-09a71d936bb0>
2013-06-27 06:25:14 +0000

    add comments and refactoring

--- trunk/framework/config/lib/AclUtility.php
+++ trunk/framework/config/lib/AclUtility.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ *
+ *
+ */
 
 class AclUtility 
 {
