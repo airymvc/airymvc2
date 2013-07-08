96e34ebb19f167637f7ea75606514bc7eeaca4a9
aaronchang.tw@gmail.com <aaronchang.tw@gmail.com@664e361d-b696-899b-ad6e-09a71d936bb0>
2013-07-08 13:02:44 +0000

    add DivElement

---
+++ trunk/framework/app/coreLib/ui/html/components/DivElement.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * AiryMVC Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ *
+ * It is also available at this URL: http://opensource.org/licenses/BSD-3-Clause
+ * The project website URL: https://code.google.com/p/airymvc/
+ *
+ * @author: Hung-Fu Aaron Chang
+ */
+
+class DivElement extends AbstractFormElement{
+
+    protected $_innerHtml;
+    private $_id;
+    
+    public function __construct($id)
+    {
+        $this->setId($id);
+    }
+        
+    public function setHtmlValue($innerHtml)
+    {
+        $this->_innerHtml = $inHtml;
+    }
+    
+    public function getHtmlValue() 
+    {
+        return $this->_innerHtml;	
+    }
+    
+    //override the method
+    protected function renderElements()
+    {
+    	$inputText = "<div";
+        foreach ($this->_attributes as $key => $value)
+        {
+            $inputText = $inputText . " " . $key ."=\"".$value ."\"";
+        }
+        $inputText = $inputText . ">";
+        
+        $this->_elementText = $inputText . $this->_innerHtml . '</div>';       
+    }
+    
+}
+
