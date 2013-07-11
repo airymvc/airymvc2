41941d39d95c47e2f0045945cafbf30a29632d49
aaronchang.tw <aaronchang.tw@gmail.com>
2013-07-11 09:09:12 +0000

    ACL refactoring

--- app/coreLib/acl/AclComponent.php
+++ app/coreLib/acl/AclComponent.php
@@ -19,16 +19,29 @@ require_once ('AclUtility.php');
 class AclComponent {
 	
 	const MD5 = "MD5";
+	
+	public $_loginForm;
+	//put form varriable here
+	private $_moduleName; 
+	private $_formId; 
+	private $_formName;
+	private $_uidLabel;
+	private $_pwdLabel; 
+	private $_formLayout = array(); 
+	private $_loginMsgId;
     
-    /**
+	
+
+
+	/**
      * signIn, check with the database table with uid, pwd and mapping table
      * @params: String $uid, String $pwd, String $mapTbl
      */
     public function signIn($moduleName = null, $controllerName = null, $actionName = null) {
 
-        $moduleName = MvcReg::getModuleName();
-        $controllerName = MvcReg::getControllerName();
-        $actionName = MvcReg::getActionName();
+        $moduleName = (is_null($moduleName)) ? MvcReg::getModuleName() : $moduleName;
+        $controllerName = (is_null($controllerName)) ? MvcReg::getControllerName() : $controllerName;
+        $actionName = (is_null($actionName)) ? MvcReg::getActionName() : $actionName;
         
         $acl = AclUtility::getInstance();
         $tbl_id = $acl->getTableIdByModule($moduleName);
@@ -49,8 +62,9 @@ class AclComponent {
         $dbIsdelete = (isset($mapFields["is_delete"])) ? $mapFields["is_delete"] : null;
         $dbIsdeleteValue = (isset($mapFields["is_delete_value"]) && !is_null($dbIsdelete)) ? $mapFields["is_delete_value"] : null;
 
-        $uid = $this->params["{$dbUid}"];
-        $pwd = $this->params["{$dbPwd}"];
+        $params = Parameter::getParams();
+        $uid = $params["{$dbUid}"];
+        $pwd = $params["{$dbPwd}"];
 
         $mysql_results = null;
         //determine use encryption for password or not 
@@ -97,7 +111,7 @@ class AclComponent {
             $successfulController = $successfulArray[$moduleName]['controller'];
             $successfulAction = $successfulArray[$moduleName]['action'];
             //forward to login sucessful action - this is set in the act.xml
-            Dispatcher::forward($moduleName, $successfulController, $successfulAction, $this->params);
+            Dispatcher::forward($moduleName, $successfulController, $successfulAction, $params);
         } else {
             $authArray = $acl->getAuthentications();
             $loginErrorAction = "loginErrorAction";
@@ -110,15 +124,28 @@ class AclComponent {
         }
     }
     
+    public function loginOut() {
+        $moduleName = MvcReg::getModuleName();
+        unset($_SESSION[$moduleName][Authentication::UID]);
+        unset($_SESSION[$moduleName][Authentication::ENCRYPT_UID]);
+        unset($_SESSION[$moduleName][Authentication::IS_LOGIN]);
+        unset($_SESSION[Authentication::UID]['module']);
+    }
     
-    public function prepareLoginForm($moduleName = null, $formId = null, $formName = null ,$uidLabel = null, $pwdLabel = null, $formLayout = null, $loginMsgId = null) {
+    /**
+     * Modify the following function
+     */
+    
+    //getter and setter for each form variables
+    
+    public function prepareLoginForm() {
 
-        $moduleName = (is_null($moduleName)) ? MvcReg::getModuleName() : $moduleName;
-        $formName   = (is_null($formName)) ? "system_login_form" : $formName;
-        $formId     = (is_null($formId)) ? "system_login_form" : $formId;
-        $uidLabel   = (is_null($uidLabel)) ? "%{Username}%" : $uidLabel;
-        $pwdLabel   = (is_null($uidLabel)) ? "%{Password}%" : $pwdLabel;
-        $loginMsgId = (is_null($loginMsgId)) ? "system_login_message" : $loginMsgId; 
+        $this->_moduleName = (is_null($this->_moduleName)) ? MvcReg::getModuleName() : $this->_moduleName;
+        $this->_formName   = (is_null($this->_formName)) ? "system_login_form" : $this->_formName;
+        $this->_formId     = (is_null($this->_formId)) ? "system_login_form" : $this->_formId;
+        $this->_uidLabel   = (is_null($this->_uidLabel)) ? "%{Username}%" : $this->_uidLabel;
+        $this->_pwdLabel   = (is_null($this->_pwdLabel)) ? "%{Password}%" : $this->_pwdLabel;
+        $this->_loginMsgId = (is_null($this->_loginMsgId)) ? "system_login_message" : $this->_loginMsgId; 
         
    		 /**
      	  * FormLayout example:
@@ -130,22 +157,189 @@ class AclComponent {
      	  *       {elementId} => array('{open_html}, {close_html})
      	  *      );
      	  *      
-     	  */            
-        $loginForm = new LoginForm($moduleName, $formId, $formName, $uidLabel, $pwdLabel, $formLayout, $loginMsgId);
+     	  */ 
+        $this->_formLayout = array($this->_formId  => array("<div class='{$this->_formName}' name='{$this->_formName}'", "</div>"));           
+        $loginForm = new LoginForm($this->_moduleName, $this->_formId, $this->_formName, $this->_uidLabel, $this->_pwdLabel, $this->_formLayout, $this->_loginMsgId);
         
         return $loginForm;
     }
     
+    protected function login($loginFormName = null) {
+        $this->_loginForm = $this->prepareLoginForm();
+    	$loginFormName = (is_null($loginFormName)) ? $this->_loginForm->getFormId() : $loginFormName;
+        //to generate the view
+        $moduleName     = MvcReg::getModuleName();
+        $controllerName = MvcReg::getControllerName();
+        $actionName     = MvcReg::getActionName();
+        $params         = Parameter::getParams();
+        $variables = array($loginFormName => $this->_loginForm);
+        Dispatcher::forward($moduleName, $controllerName, $actionName, $params, $variables);
+    } 
+
     
 
     
-    public function loginOut() {
-        $moduleName = MvcReg::getModuleName();
-        unset($_SESSION[$moduleName][Authentication::UID]);
-        unset($_SESSION[$moduleName][Authentication::ENCRYPT_UID]);
-        unset($_SESSION[$moduleName][Authentication::IS_LOGIN]);
-        unset($_SESSION[Authentication::UID]['module']);
+    public function resetLoginForm($moduleName = null, $formId = null, $formName = null, $uidLabel = null, $pwdLabel = null, $formLayout = null, $loginMsgId = null) {
+        $this->setLoginFormOptions($moduleName, $formId, $formName, $uidLabel, $pwdLabel, $formLayout, $loginMsgId);
+    	$this->_loginForm = $this->prepareLoginForm($moduleName, $formId, $formName, $uidLabel, $pwdLabel, $formLayout, $loginMsgId);
+        return $this->_loginForm;
     }
     
+    
+    protected function loginError($errorMessage = null, $errorMsgVariableName = null, $loginFormName = null) {
+        $this->_loginForm->populateErrorMessage($errorMessage);
+        $loginFormName = (is_null($loginFormName)) ? $this->_loginForm->getFormId() : $loginFormName;
+        
+        $errorMessage = is_null($errorMessage) ? "ERROR!!" : $errorMessage;
+        $errorMsgVariableName = is_null($errorMsgVariableName) ? 'loginErrorMessage' : $errorMsgVariableName;
+        
+        $variables = array($loginFormName => $this->_loginForm,
+                           $errorMsgVariableName => $errorMessage
+                          );
+        
+        $moduleName     = MvcReg::getModuleName();
+        $controllerName = MvcReg::getControllerName();
+        $actionName     = MvcReg::getActionName();
+        $params         = Parameter::getParams();
+        Dispatcher::forward($moduleName, $controllerName, $actionName, $params, $variables);
+    }
+    
+    /**
+     * This is the convenient method for setup all the form variables
+     * @param String $moduleName
+     * @param String $formId
+     * @param String $formName
+     * @param String $uidLabel
+     * @param String $pwdLabel
+     * @param array $formLayout
+     * @param String $loginMsgId
+     */
+    public function setLoginFormOptions($moduleName = null, $formId = null, $formName = null ,$uidLabel = null, $pwdLabel = null, $formLayout = null, $loginMsgId = null) {
+		$this->setModuleName($moduleName);
+		$this->setFormId($formId);
+		$this->setFormName($formName);
+		$this->setLoginMsgId($loginMsgId);
+		$this->setPwdLabel($pwdLabel);
+		$this->setUidLabel($uidLabel);
+		$this->setFormLayout($formLayout);
+    }
+    
+    
+	/**
+	 * @return the $_loginForm
+	 */
+	public function getLoginForm() {
+		return $this->_loginForm;
+	}
+
+	/**
+	 * @return the $_moduleName
+	 */
+	public function getModuleName() {
+		return $this->_moduleName;
+	}
+
+	/**
+	 * @return the $_formId
+	 */
+	public function getFormId() {
+		return $this->_formId;
+	}
+
+	/**
+	 * @return the $_formName
+	 */
+	public function getFormName() {
+		return $this->_formName;
+	}
+
+	/**
+	 * @return the $_uidLabel
+	 */
+	public function getUidLabel() {
+		return $this->_uidLabel;
+	}
+
+	/**
+	 * @return the $_pwdLabel
+	 */
+	public function getPwdLabel() {
+		return $this->_pwdLabel;
+	}
+
+	/**
+	 * @return the $_formLayout
+	 */
+	public function getFormLayout() {
+		return $this->_formLayout;
+	}
+
+	/**
+	 * @return the $_loginMsgId
+	 */
+	public function getLoginMsgId() {
+		return $this->_loginMsgId;
+	}
+
+	/**
+	 * @param Form $_loginForm
+	 */
+	public function setLoginForm($loginForm) {
+		$this->_loginForm = $loginForm;
+	}
+
+	/**
+	 * @param String $moduleName
+	 */
+	public function setModuleName($moduleName) {
+		$this->_moduleName = $moduleName;
+	}
+
+	/**
+	 * @param String $formId
+	 */
+	public function setFormId($formId) {
+		$this->_formId = $formId;
+	}
+
+	/**
+	 * @param String $formName
+	 */
+	public function setFormName($formName) {
+		$this->_formName = $formName;
+	}
+
+	/**
+	 * @param String $uidLabel
+	 */
+	public function setUidLabel($uidLabel) {
+		$this->_uidLabel = $uidLabel;
+	}
+
+	/**
+	 * @param String $pwdLabel
+	 */
+	public function setPwdLabel($pwdLabel) {
+		$this->_pwdLabel = $pwdLabel;
+	}
+
+	/**
+	 * @param array() $_formLayout
+	 */
+	public function setFormLayout($formLayout) {
+		$this->_formLayout = $formLayout;
+	}
+
+	/**
+	 * @param String $loginMsgId
+	 */
+	public function setLoginMsgId($loginMsgId) {
+		$this->_loginMsgId = $loginMsgId;
+	}
+
+
+
+	
 }
+
 ?>
---
+++ core/Parameter.php
@@ -0,0 +1,111 @@
+<?php
+
+class Parameter {
+    /**
+     * This is used for save the varialbe into an array
+     * @var array 
+     */
+    public static $_params = array();
+    
+    /**
+     * setter
+     * @param array $params 
+     */
+    public static function setParams($params) {
+        
+        self::$_params = $params;
+    }
+    
+    /**
+     * getter
+     * @return array 
+     */
+    public static function getParams() {
+        
+        return self::$_params;
+    }
+    
+    /**
+     * setter
+     * 
+     * @param string $key
+     * @param string $value 
+     */
+    public static function setParam($key, $value) {
+        
+        self::$_params[$key] = $value;
+    }
+    
+    /**
+     * getter
+     * 
+     * @param string $key
+     * @return string 
+     */
+    public static function getParam($key) {
+        
+        return self::$_params[$key];
+    }
+    
+    /**
+     * set params to Session
+     */
+    public static function setSession($params) {
+    	foreach ($params as $key => $value) {
+    		$_SESSION[$key] = $value;
+    	}
+    }
+
+    /**
+     * unset params to Session
+     */
+    public static function unsetSession($params) {
+    	foreach ($params as $key => $value) {
+    		unset($_SESSION[$key]);
+    	}
+    }    
+    
+    /**
+     * get parameter from Session
+     */
+    public static function getSession($key) {
+    	return $_SESSION[$key];
+    } 
+    
+    /**
+     * get parameter from Session
+     */
+    public static function getModuleSession($key) {
+    	$moduleName = MvcReg::getModuleName();
+    	return $_SESSION[$moduleName][$key];
+    }
+    
+    /**
+     * set params to Session
+     */
+    public static function setModuleSession($params) {
+    	$moduleName = MvcReg::getModuleName();
+    	foreach ($params as $key => $value) {
+    		$_SESSION[$moduleName][$key] = $value;
+    	}
+    }
+    
+    /**
+     * set params to Session
+     */
+    public static function unsetModuleSession($params) {
+    	$moduleName = MvcReg::getModuleName();
+    	foreach ($params as $key => $value) {
+    		unset($_SESSION[$moduleName][$key]);
+    	}
+    }
+
+    /**
+     * set params to Session
+     */
+    public static function unsetAllParams() {
+    	foreach ($this->_params as $key => $value) {
+    		unset($this->_params[$key]);
+    	}
+    }
+}
