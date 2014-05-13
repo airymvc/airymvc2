74d17b0335d727f652aaf74f5a93d043bc9d0354
aaronchang.tw <aaronchang.tw@gmail.com>
2014-05-13 09:56:14 +0000

    resolve the url path

---
+++ core/RouterUrlRewriter.php
@@ -0,0 +1,39 @@
+<?php
+class RouterUrlRewriter {
+	
+	public function remapGetAndPost($actionKeyword) {
+		
+		//Deal with $_GET for parsing module/controller/action/querystring 
+		$actionWithQueryPath = $_GET[$actionKeyword];
+		$params = explode("/", $actionWithQueryPath);
+		$GETParams = array();
+		for ($i=1; $i<count($params); $i=$i+2) {
+			$GETParams[$params[$i]] = $params[$i+1];
+		}
+		//deal with action
+		$GETParams[$actionKeyword] = $params[0];
+		foreach ($GETParams as $key=>$value) {
+			$_GET[$key] = $value;
+		}
+			
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$qstringPieces = explode('&', $_SERVER['QUERY_STRING']);
+		    $newQueryString = "";
+			foreach ($qstringPieces as $piece) {
+				$kv = explode("=", $piece);
+				if ($kv[0] == $actionKeyword) {
+					$params = explode("/", $kv[1]);
+					$newQueryString .= $actionKeyword . "=" . $params[0] . "&";;
+					for ($i=1; $i<count($params); $i=$i+2) {
+						$newQueryString .= $params[$i] . "=" . $params[$i+1] . "&";
+					}
+				} else {
+					$newQueryString .= $piece . "&";					
+				}
+			}
+			$_SERVER['QUERY_STRING'] = rtrim($newQueryString, "&");
+		}
+		
+	}
+	
+}
